d05f186685e5c306e167652fb31f1fc011a21344
MRXGhost22 <venkateshrathod@242002@gmail.com>
2023-09-21 01:59:04 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+//! IMPORTS FROM PACKAGES
+const express = require("express");
+const mongoose = require("mongoose");
+
+//! IMPORTS FROM OTHER FILES
+const authRouter = require("./routes/auth");
+const adminRouter = require("./routes/admin");
+const productRouter = require("./routes/product");
+const userRouter = require("./routes/user");
+
+//! INIT
+const PORT = process.env.PORT ||  3000;
+const app = express();
+const DB =
+  "mongodb+srv://venkatesh242002:Venkatesh123@cluster0.qbqbtcd.mongodb.net/?retryWrites=true&w=majority";
+
+//! Middleware
+//CLIENT ->middleware ->SERVER ->CLIENT
+app.use(express.json());
+app.use(authRouter);
+app.use(adminRouter);
+app.use(productRouter);
+app.use(userRouter);
+
+//! Connections
+//GET, PUT, POST, DELETE, UPDATE -> CRUD
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log("Connection Successful");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`connecting at port ${PORT}`);
+});
